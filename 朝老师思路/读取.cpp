805bb179182b32cd8886d6eda8626afaeae9a896
20171106577 <1792100080@qq.com>
2020-06-17 18:19:26 +0800

    6.17
    加了些注释

--- 朝老师思路/读取.cpp
+++ 朝老师思路/读取.cpp
@@ -4,11 +4,27 @@
 struct stu_info
 {
     char num[20];
-	char name[20];
-	char sex[3];
-	char phone_no[12];
-	float math;
-	float phy;	
+    char years[20];
+	char scl[20];
+	char place[20];
+	char classes[20];
+	char class_num[20];
+	char class_name[20];
+	char class_plan[20];
+	float grade[20];
+	float start_grade[20];
+	char class_quality[20];//性质 
+	char class_nature[20];//属性 
+	float time [20];
+	float class_grade;
+	char class_place[20];
+	char person[20];
+	char classes_nature[20];	
+	char test[20];
+	char test_way[20];
+	char classes_again[20];
+	char classes_classes[20];//学位课程
+	char classes_assisst[20]; //辅修课程 
 };
 
 int main()
@@ -31,26 +47,31 @@ int main()
 	strncpy(student.num,str,temp);
 	oldPos=temp;
 	temp=strchr(str+oldPos+1,',')-str;
-	strncpy(student.name,str+oldPos+1,temp-oldPos-1);
-	student.name[temp-oldPos-1]=0;
+	strncpy(student.num,str+oldPos+1,temp-oldPos-1);
+	student.num[temp-oldPos-1]=0;
 	oldPos=temp;
 	temp=strchr(str+oldPos+1,',')-str;
-	strncpy(student.sex,str+oldPos+1,temp-oldPos-1);
-	student.sex[2]='\0';
+	strncpy(student.years,str+oldPos+1,temp-oldPos-1);
+	student.years[2]='\0';
 	oldPos=temp;
 	temp=strchr(str+oldPos+1,',')-str;
-	strncpy(student.phone_no,str+oldPos+1,temp-oldPos-1);
-	student.phone_no[temp-oldPos]='\0';
+	strncpy(student.scl,str+oldPos+1,temp-oldPos-1);
+	student.scl[temp-oldPos]='\0';
 	oldPos=temp;
 	temp=strchr(str+oldPos+1,',')-str;
 	strncpy(strtemp,str+oldPos+1,temp-oldPos-1);
 	strtemp[temp-oldPos-1]='\0';
-	student.math=atof(strtemp);
+	student.place=atof(strtemp);
 	oldPos=temp;
 	strncpy(strtemp,str+oldPos+1,temp-oldPos-1);
 	strtemp[temp-oldPos-1]='\0';
-	student.phy=atof(strtemp);
-	printf("%s %s %s %s %f %f\n",student.num,student.name,student.sex,student.phone_no,student.math,student.phy);
+	student.classes=atof(strtemp);
+	//printf("%s %s %s %s %f %f\n",student.num,student.years,student.scl,student.place,student.classes,student.class_num,student.class_name,student.class_plan,student.grade,student.start_grade,student.class_quality,student.class_nature,);
+	
+   
+	
+
+	 
 	fclose(fp);
 	return 0;
 }
